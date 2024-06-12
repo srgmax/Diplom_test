85a36a0bef12278bd7350a5fd0111ed122a991b7
srgmax <sergio2218@yandex.ru>
2024-06-12 22:25:50 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,12 +1,12 @@
 import config
 import requests
-import data
+
 
 # Создание нового заказа
 def post_new_order(body):
     return requests.post(config.URL_SERVICE + config.CREATE_ORDER,
                          json=body)
 
-def details_order():
-    track = post_new_order(data.order_body).json().get("track")
-    return requests.get(config.URL_SERVICE + config.GET_ORDER + str(track))
+
+def details_order(trackOrder):
+    return requests.get(config.URL_SERVICE + config.GET_ORDER + str(trackOrder))

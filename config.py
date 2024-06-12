2f0b95783c1ccc9b2e007df1c90a441f47398f74
srgmax <sergio2218@yandex.ru>
2024-06-12 22:25:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://9a0d18b4-9cc2-4c84-b391-cc43c6939f75.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://dc38bf0b-bb01-4b2c-becc-699fbc8df912.serverhub.praktikum-services.ru"
 CREATE_ORDER = "/api/v1/orders/"
 GET_ORDER = "/api/v1/orders/track?t="

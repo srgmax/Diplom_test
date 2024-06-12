1b51ff4d756f96e897578620fe633014aff6b1f5
srgmax <sergio2218@yandex.ru>
2024-06-12 22:26:12 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 # Сергей Лобанов, 17-я когорта — Финальный проект. Инженер по тестированию плюс
-import sender_stand_request
 
+import sender_stand_request
+import data
 
 def test_response_code():
-    order_responce = sender_stand_request.details_order().status_code
+    track = sender_stand_request.post_new_order(data.order_body).json().get("track")
+    order_responce = sender_stand_request.details_order(track).status_code
     assert order_responce == 200
